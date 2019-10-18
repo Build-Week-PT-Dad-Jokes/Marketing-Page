51a9eca6127b4c5b6d5699a0f8dab02b7328fc08
KJ Schneider <kjdschneider@gmail.com>
2019-10-17 20:57:31 -0400

    Updated readme w/ Canvas, continued work on landing page

---
+++ landing.js
@@ -0,0 +1,23 @@
+let slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n){
+    showDivs(slideIndex += n);
+}
+
+
+function showDivs(n){
+    let slides = document.getElementsByClassName('slideShow');
+    if (n > slides.length){
+        slideIndex = 1;
+    }
+    if (n < 1){
+        slideIndex = slides.length;
+    }
+    for (let i=0; i<slides.length; i++){
+        slides[i].style.display = 'none';
+    }
+    slides[slideIndex-1].style.display = 'block';
+}
+
+
