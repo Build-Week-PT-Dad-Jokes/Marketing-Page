bd8f05ed0190b701d3d0bb8dee4c9951880d06f9
KJ Schneider <kjdschneider@gmail.com>
2019-10-19 16:44:22 -0400

    Various changes to background, carousel, landing page

--- landing.js
+++ landing.js
@@ -1,21 +1,25 @@
 let slideIndex = 1;
 showDivs(slideIndex);
 
-function plusDivs(n){
-    showDivs(slideIndex += n);
+function plusDivs(num){
+    showDivs(slideIndex += num);
 }
 
 
-function showDivs(n){
-    let slides = document.getElementsByClassName('slideShow');
-    if (n > slides.length){
+
+
+function showDivs(num){
+    const slides = document.getElementsByClassName('slideShow');
+    console.log(slides);
+    if (num > slides.length){
         slideIndex = 1;
     }
-    if (n < 1){
+    if (num < 1){
         slideIndex = slides.length;
     }
     for (let i=0; i<slides.length; i++){
         slides[i].style.display = 'none';
+        console.log('for loop i --->' + i);
     }
     slides[slideIndex-1].style.display = 'block';
 }
